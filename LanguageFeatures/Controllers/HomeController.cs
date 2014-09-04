588c8f6b10860296a4248b1b5377062fc9b646c8
arahgozar <arahgozar@gmail.com>
2014-09-04 11:53:18 -0400

    First Commit

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Navigate to a URL to show an example";
+        }
+        public ViewResult AutoProperty()
+        {
+            // create a new Product object
+            Product myProduct = new Product();
+            // set the property value
+            myProduct.Name = "Kayak";
+            // get the property
+            string productName = myProduct.Name;
+            // generate the view
+            return View("Result",
+            (object)String.Format("Product name: {0}", productName));
+        }
+        public ViewResult CreateProduct()
+        {
+            // create a new Product object
+            Product myProduct = new Product();
+            // set the property values
+            myProduct.ProductID = 100;
+            myProduct.Name = "Kayak";
+            myProduct.Description = "A boat for one person";
+            myProduct.Price = 275M;
+            myProduct.Category = "Watersports";
+            return View("Result",
+            (object)String.Format("Category: {0}", myProduct.Category));
+        }
+
+    }
+}
